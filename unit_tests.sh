8714e40e66db467b875a725fb7bdba2206c8c7fa
jbertin <jmbcorp_house@hotmail.com>
2023-12-26 21:42:12 +0100

    unit test improved

--- unit_tests.sh
+++ unit_tests.sh
@@ -1,39 +1,86 @@
 #!/bin/bash
 
-# Chemin vers le script Python
 PYTHON_SCRIPT="main.py"
 
-# Dossier contenant les cas de test
-TEST_ERROR_FOLDER="./test_error_cases"
-
-TEST_GOOD_FOLDER="./test_good_cases"
+TEST_ERROR_FOLDER="./unit_tests/test_error_cases"
+TEST_MANDATORY_FOLDER="./unit_tests/test_mandatory_cases"
+TEST_EXPECTED_FOLDER="./unit_tests/test_expected_output"
+TEST_OPTIONNAL_FOLDER="./unit_tests/test_optionnal_cases"
 
 # Couleurs
 RED='\033[0;31m'
 GREEN='\033[0;32m'
+BLUE='\033[0;34m'
 NO_COLOR='\033[0m'
 
+echo -e "${BLUE}You can run this program with the options 'errors', 'optional', 'mandatory', or 'all'.${NO_COLOR}"
+echo ""
 
-for test_file in $TEST_ERROR_FOLDER/*.txt; do
-    test_case=$(basename "$test_file")
-    echo "Testing $test_case..."
-    python3 $PYTHON_SCRIPT "$test_file"
-    if [ $? -eq 1 ]; then
-        echo -e "${GREEN}Test Passed${NO_COLOR}"
-    else
-        echo -e "${RED}Test Failed${NO_COLOR}"
-    fi
-    echo ""
-done
+compare_output() {
+    expected_output_file="$1"
+    actual_output_file="$2"
 
-for test_file in $TEST_GOOD_FOLDER/*.txt; do
-    test_case=$(basename "$test_file")
-    echo "Testing $test_case..."
-    python3 $PYTHON_SCRIPT "$test_file"
+    sort "$expected_output_file" > temp_expected_output.txt
+    sort "$actual_output_file" > temp_actual_output.txt
+
+    diff -B -w --color -y "temp_expected_output.txt" "temp_actual_output.txt" > /dev/null
     if [ $? -eq 0 ]; then
-        echo -e "${GREEN}Test Passed${NO_COLOR}"
+        echo -e "${GREEN}Output Matched${NO_COLOR}"
     else
-        echo -e "${RED}Test Failed${NO_COLOR}"
+        echo -e "Expected      | Actual"
+        diff -B -w --color -y -W 30 "temp_expected_output.txt" "temp_actual_output.txt"
+        echo -e "${RED}Output Mismatch${NO_COLOR}"
     fi
-    echo ""
-done
+}
+
+
+error_tests() {
+    for test_file in $TEST_ERROR_FOLDER/*.txt; do
+        echo "Testing $test_case..."
+        python3 $PYTHON_SCRIPT "$test_file"
+        if [ $? -eq 1 ]; then
+            echo -e "${GREEN}Test Passed${NO_COLOR}"
+        else
+            echo -e "${RED}Test Failed${NO_COLOR}"
+        fi
+        echo ""
+    done
+}
+
+mandatory_tests() {
+    for test_file in $TEST_MANDATORY_FOLDER/*.txt; do
+        echo "----------------------------------------"
+        test_case=$(basename "$test_file")
+        echo "Testing $test_case..."
+        python3 $PYTHON_SCRIPT "$test_file" > temp_output.txt
+
+        if [ $? -eq 0 ]; then
+            compare_output "${TEST_EXPECTED_FOLDER}/${test_case}" "temp_output.txt"
+        else
+            echo -e "${RED}Test Failed${NO_COLOR}"
+        fi
+    done
+    rm temp_output.txt temp_expected_output.txt temp_actual_output.txt
+}
+
+case "$1" in
+    errors)
+        error_tests
+        ;;
+    optional)
+        optional_tests
+        ;;
+    mandatory)
+        mandatory_tests
+        ;;
+    all)
+        error_tests
+        mandatory_tests
+        optional_tests
+        ;;
+    *)
+        echo -e "You did not specify a valid option. So only mandatory tests will be run."
+        echo ""
+        mandatory_tests
+        ;;
+esac
